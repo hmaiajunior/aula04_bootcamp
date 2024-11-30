a670b0400f015756526b5f82a338ad5fe0155694
hmaiajunior <juniorce04@gmail.com>
2024-11-30 10:21:23 -0300

    resolvendo exercicio 01

---
+++ exercicios.py
@@ -0,0 +1,8 @@
+# Exercícios
+
+# 01 - Crie uma lista com os números de 1 a 10 e use um loop para imprimir cada número elevado ao quadrado.
+
+numeros = list(range(1,11))
+
+for i in numeros:
+    print(i ** 2)
