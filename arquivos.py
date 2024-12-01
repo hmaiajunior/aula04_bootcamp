dff1594119cdafd8a114658e182f8c389946a566
hmaiajunior <juniorce04@gmail.com>
2024-12-01 10:21:07 -0300

    Lendo um arquivo csv

---
+++ arquivos.py
@@ -0,0 +1,24 @@
+# Para ler um CSV em python podemos utilizar um módulo nativo do Python chamado de CSV. Segue exemplo abaixo:
+
+
+import csv
+
+# caminho para o arquivo csv
+caminho_arquivo = 'exemplo.csv'
+
+# Inicializa uma lista para armazenar os dados
+dados = []
+
+# Usa o gerenciador de contexto `with` para abrir o arquivo
+with open(caminho_arquivo, mode='r', encoding='utf-8') as arquivo:
+    # Cria um objeto leitor de CSV
+    leitor_csv = csv.DictReader(arquivo)
+    
+    # Itera sobre as linhas do arquivo CSV
+    for linha in leitor_csv:
+        # Adiciona cada linha (um dicionário) à lista de dados
+        dados.append(linha)
+
+# Exibe os dados lidos do arquivo CSV
+for registro in dados:
+    print(registro)
--- exercicios.py
+++ exercicios.py
@@ -8,20 +8,39 @@
 #     print(i ** 2)
 
 
-# 03 Crie um dicionário para armazenar informações de um livro, incluindo título, autor e ano de publicação. Imprima cada informação
+# # 03 Crie um dicionário para armazenar informações de um livro, incluindo título, autor e ano de publicação. Imprima cada informação
     
 
-informacoes_livro: dict = {
+# informacoes_livro: dict = {
     
-    "titulo": "Nosso título", 
-    "Autor": "John Arias", 
-    "ano": "2023"
-    }
+#     "titulo": "Nosso título", 
+#     "Autor": "John Arias", 
+#     "ano": "2023"
+#     }
 
-# Abaixo mostro as formas de imprir o ítem como um todo (chave - valor) e como imprimir separadamente a chave e o valor
+# # Abaixo mostro as formas de imprir o ítem como um todo (chave - valor) e como imprimir separadamente a chave e o valor
 
-for item in informacoes_livro.items():
-    print(f"{item}")
+# for item in informacoes_livro.items():
+#     print(f"{item}")
 
-# for chave, valor in informacoes_livro.items():
-#     print(f"{chave}: {valor}")
+# # for chave, valor in informacoes_livro.items():
+# #     print(f"{chave}: {valor}")
+
+
+
+
+# # 05 Dada a lista ["maçã", "banana", "cereja"] e o dicionário {"maçã": 0.45, "banana": 0.30, "cereja": 0.65}, calcule o preço total da lista de compras.
+
+# # lista01: list = ["maçã", "banana", "cereja"]
+
+# # dicionario01: dict = {"maçã": 0.45, "banana": 0.30, "cereja": 0.65}
+
+# # total = sum(dicionario01[item] for item in lista01)
+# # print(f"Preço total: {total}")
+
+# lista_compras = ["maçã", "banana", "cereja"]
+# precos = {"maçã": 0.45, "banana": 0.30, "cereja": 0.65}
+
+# total = sum(precos[item] for item in lista_compras)
+
+# print(f"Preço total: {total}")
